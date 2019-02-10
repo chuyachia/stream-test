9d7042db5f4c1b8e19266daefc5674e9b786750c
Chu-Ya Chia <chuyachia@gmail.com>
2019-02-10 10:40:46 -0500

    push null to node readable stream to trigger on end

--- public/index.js
+++ public/index.js
@@ -1,21 +1,13 @@
 const decoder = new TextDecoder();
-const writer = new WritableStream({
-	write(chunk) {
-		return new Promise((resolve, reject)=>{
-			console.log(decoder.decode(chunk));
-			resolve();
-		})
-	}
-});
+
 (function(){
 	const dest = document.getElementById('withoutWriter'); 
-	fetch('/test')
-		.then(response=> response.text())
-		.then(text=> {
-			console.log(text);
-			var node = document.createTextNode(text);
-			dest.appendChild(node);
-		})
+	fetch('/HelloWorldStream')
+		.then(response =>response.text())
+	  .then(text=> {
+	  	var node = document.createTextNode(text);
+	  	dest.appendChild(node);
+	  })
 		.catch(err=>console.log(err))
 })();
 
@@ -31,52 +23,7 @@ const writer = new WritableStream({
 		}
 	});
 	fetch('/HelloWorldStream')
-		.then(response=> response.body)
+		.then(response=> response.body)// response body is a readable stream so can be piped to writable stream
 		.then(body=> body.pipeTo(writer))
 		.catch(err=>console.log(err))
 })();
-//fetch('/HelloWorldStream')
-//	.then(response=> response.text())
-//	.then(text =>console.log(text)) 
-//.then(function(response) {
-//  var reader = response.body.getReader();
-//  var bytesReceived = 0;
-//
-//  reader.read().then(function processResult(result) {
-//    if (result.done) {
-//      console.log("Fetch complete");
-//      return;
-//    }
-//console.log(decoder.decode(result.value,{stream:true}));
-//    return reader.read().then(processResult);
-//  });
-//})	
-//	.then(response=>response.body)
-//	.then(body=>{
-//		const reader = body.getReader();
-//		var interval;
-//		return new ReadableStream({
-//			start(controller) {
-//				interval = setInterval(()=>{
-//					reader.read().then(({done,value})=>{
-//				 		if (done) {
-//							controller.close();
-//							clearInterval(interval);
-//							return;
-//						}	
-//						controller.enqueue(value);
-//					})	
-//				},1000);
-//			},
-//			cancel() {
-//				clearInterval(interval);
-//			}
-//		})
-//	})
-//.then(body=> body.pipeTo(writer))
-//	.catch(err=>console.log(err))
-
-//fetch('/HelloWorld')
-//  .then(response=>response.text())
-//	.then(text => console.log(text))
-//	.catch(err=>console.log(err))
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 const { Readable } = require('stream');
 const port = 3000;
 
@@ -11,47 +10,25 @@ app.get('/HelloWorldStream', (req,res)=> {
 	var interval;
   const readables = new Readable({
   	read(size) {
-				this.push(message[this.i++]);
+			this.push(message[this.i++]);
   	 	if (this.i >= message.length)
-  	 	 	return null;
+  	 	 	this.push(null);
   	}	
   })
 	readables.i = 0;
 
-	//readables.push(message);
-	//readables.push(null);
-	//readables.on('readable', ()=>{
-	//	let data;
-	//	while (data=readables.read(2)) {
-	//		console.log(data);
-	//		res.write(data);
-	//	}
-	//});
-		readables.on('data',(chunk)=>{
-			console.log(chunk.length);
-			res.write(chunk);
-			readables.pause();
-			setTimeout(()=> {
+	readables.on('data',(chunk)=>{
+		res.write(chunk);
+		readables.pause();
+		setTimeout(()=> {
 			readables.resume();	
-			},500)
-		})
-		readables.on('end',()=>{
-			res.end();
-		})
+		},500)
+	})
+	readables.on('end',()=>{
+		res.end();
+	})
+	// pipe equivalent on on('event')
 	//readables.pipe(res);
 });
-app.get('/HelloWorld', (req,res)=>{
-	res.send(message);
-});
-app.get('/test',(req,res)=>{
-	var i=0;
-	const interval = setInterval(()=>{
-		if(i<message.length) {
-			res.write(message[i++]);
-		} else {
-			clearInterval(interval);
-			res.end();
-		}
-	},500)
-});
+
 app.listen(port, ()=>console.log(`App listening on port ${port}`));
